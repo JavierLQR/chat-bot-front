8580eafd8d5869d65d832ceee5896f8909229383
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-16 18:13:39 -0500

    Add Pusher authentication and channel subscription to home page

--- src/app/api/pusher/auth/route.ts
+++ src/app/api/pusher/auth/route.ts
@@ -1,12 +1,21 @@
+import { methodsAxios } from '@/common/adapters/adapters'
+import { PUSHER_AUTH } from '@/common/routes/route.path.service'
 import { getAxiosError } from '@/helpers/is-axios-error'
-import axios from 'axios'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
+  console.log({
+    message: 'POST',
+    body,
+  })
 
   try {
-    const response = await axios
+    const response = await methodsAxios.POST(PUSHER_AUTH)
+
+    console.log({
+      response,
+    })
   } catch (error) {
     const { data, isAxiosError, message, status } = getAxiosError(error)
     console.error({ data, isAxiosError, message, status })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,19 @@
+'use client'
+import { useAuthPusherChannel } from '@/hooks/authPusherChannel'
+import { useInitialAuthPusher } from '@/hooks/initialAuthPusher'
+
 const Home = () => {
+  const pusher = useInitialAuthPusher()
+
+  useAuthPusherChannel<{ message: string }>({
+    callback: (data) => {
+      console.log({ data })
+    },
+    channelName: 'test-channel',
+    eventName: 'test-event',
+    pusher,
+  })
+
   return (
     <div>
       <h1>Home</h1>
