e4daf5ff86e5632c394d88751e5aa5724038e7c8
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-17 19:31:51 -0500

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useAuthPusherChannel } from '@/hooks/authPusherChannel'
 import { useInitialAuthPusher } from '@/hooks/initialAuthPusher'
+import axios from 'axios'
 
 const Home = () => {
   const pusher = useInitialAuthPusher()
@@ -9,13 +10,20 @@ const Home = () => {
     callback: (data) => {
       console.log({ data })
     },
-    channelName: 'test-channel',
+    channelName: 'private-test-channel',
     eventName: 'test-event',
     pusher,
   })
 
+  const test = () => {
+    axios
+      .post('http://localhost:4000/api-v1/auth/pusher/autentication')
+      .then((res) => console.log({ res }))
+  }
+
   return (
     <div>
+      <button onClick={test}>Test</button>
       <h1>Home</h1>
     </div>
   )
